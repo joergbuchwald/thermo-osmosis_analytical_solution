82b2bf34d61109d108650cf18ee0aedd7994eff1
joergbuchwald <joerg.buchwald@ufz.de>
2022-05-30 13:23:23 +0200

    rename module, set LICENSE to BSD

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@
 
 from setuptools import setup, find_packages
 
-setup(name="anasol.py",
+setup(name="zhou_solution_thermo_osmosis.py",
       version=0.1,
       maintainer="Jörg Buchwald",
       maintainer_email="joerg_buchwald@ufz.de",
       author="Jörg Buchwald",
       author_email="joerg.buchwald@ufz.de",
       url="https://github.com/joergbuchwald/thermo-osmosis_analytical_solution",
-      license="MIT -  see LICENSE.txt",
+      license="BSD -  see LICENSE.txt",
       platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
       include_package_data=True,
       install_requires=["numpy", "scipy"],
-      py_modules=["heatsource"],
+      py_modules=["zhou_solution_thermo_osmosis"],
       packages=[])
